6cd337ddc802319d6357df28405228b3bfcff7f7
Jia Earn <jiae4rn@gmail.com>
2023-12-18 20:15:45 +0800

    Projects HTML foundation

--- Javascripts/My-Projects.js
+++ Javascripts/My-Projects.js
@@ -2,23 +2,39 @@
 const ProjectsArray = [ 
     { 
         imagePath: "Images/Project_1T.jpg", 
-        companyName: "Retirement Planner"
+        projectName: "Retirement Planner",
+        projectDescription: "A tool to take control of you or your family's finances!",
+        techStack: ["ReactJS, Bootstrap, Tailwind, Firebase"]
     }, 
     { 
         imagePath: "Images/Project_2T.jpg", 
-        companyName: "Macro Enhanced Excels"
+        projectName: "Macro Enhanced Excels",
+        projectDescription: "Supercharge your mundane excel workflows!",
+        techStack: ["VBA", "MS Excel"]
     },
     { 
         imagePath: "Images/Project_3T.jpg", 
-        companyName: "Personal Finance Calendar"
+        projectName: "Personal Finance Calendar",
+        projectDescription: "One file to track all your finances & more!",
+        techStack: ["Python", "Openpyxl"]
     },
     { 
         imagePath: "Images/Project_4T.jpg", 
-        companyName: "Disk Based Frequent Item Miner"
+        projectName: "Disk Based Frequent Itemset Miner",
+        projectDescription: "Mine frequent itemsets no matter the size of your main memory!",
+        techStack: ["Python"]
     },
     { 
         imagePath: "Images/Project_1T.jpg", 
-        companyName: "Career Navigator"
+        projectName: "Trending YT Videos Predictor",
+        projectDescription: "Trained on real YT data using state of the art algorithms such as Random Forest!",
+        techStack: ["Python", "Pandas", "Scikit-Learn"]
+    },
+    { 
+        imagePath: "Images/Project_2T.jpg", 
+        projectName: "Career Navigator",
+        projectDescription: "Your digital companion to empower your tech career!",
+        techStack: ["ReactJS", "Bootstrap"]
     }
 ]; 
 
@@ -36,11 +52,35 @@ function MyProjects() {
 
         const GalleryItemElement = document.createElement("div");
         GalleryItemElement.classList = "project-section-gallery-element expand-on-hover";
-
         GalleryItemElement.appendChild(imageElement);
 
+        const titleElement = document.createElement("div");
+        titleElement.innerText = e.projectName;
+        titleElement.classList = "project-section-gallery-element-title";
+
+        const descElement = document.createElement("div");
+        descElement.innerText = e.projectDescription;
+        descElement.classList = "project-section-gallery-element-desc";
+
+        const techStackElement = document.createElement("ul");
+
+        e.techStack.forEach((e2) => {
+
+            const techP = document.createElement("li");
+            techP.innerText = e2;
+            techP.classList = "project-section-gallery-tech";
+            techStackElement.appendChild(techP);
+        })
+
+        GalleryItemElement.appendChild(titleElement);
+        GalleryItemElement.appendChild(descElement);
+        GalleryItemElement.appendChild(techStackElement);        
         
         GalleryElement.appendChild(GalleryItemElement);
+
+
+
+
         }); 
  
 
